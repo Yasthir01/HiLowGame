b601fcb8a07d9410683d4dd854ff272a59ae3294
yasthir <yasthir01@gmail.com>
2022-04-15 17:11:37 +0200

    Add project files.

---
+++ HiLowGame/HiLoGame.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HiLowGame
+{
+    static class HiLoGame
+    {
+        public const int MAXIMUM = 10;  // we are specifying the maximum range value 
+        private static Random random = new Random();
+        private static int currentNumber = random.Next(1, MAXIMUM + 1);  // we have to say MAX + 1 because random doesnt include last number
+        private static int pot = 10;  // amount of money we have in the pot to gamble with
+
+        public static int GetPot() { return pot; }  // we just want to return the current value of the pot
+
+        public static void Guess(bool higher)
+        {
+            int nextNumber = random.Next(1, MAXIMUM + 1);
+            // figure out if the user's guess is greater than or less than current number
+            if ((higher && nextNumber >= currentNumber) || (!higher && nextNumber <= currentNumber))
+            {
+                Console.WriteLine("You guessed right!");
+                pot++;  // because the user guess correctly then we add 1 to the pot
+            }
+            else
+            {
+                // the user guess incorrectly
+                Console.WriteLine("Bad luck, you guessed wrong...");
+                pot--;  // we have to deduct 1 because the user guessed incorrectly
+            }
+            currentNumber = nextNumber;  // the new current number will be the next number
+            Console.WriteLine($"The current number is {currentNumber}");
+        }
+
+        public static void Hint()
+        {
+            int half = MAXIMUM / 2;
+            if (currentNumber >= half)
+                Console.WriteLine($"The number is atleast {half}");
+            else Console.WriteLine($"The number is at most {half}");
+            pot--;  // because we used a hint we have to deduct 1 from the pot
+        }
+    }
+}
---
+++ HiLowGame/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HiLowGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // initial message on the screen that the user sees
+            Console.WriteLine("Welcome to HiLo");
+            Console.WriteLine($"Guess numbers between 1 and {HiLoGame.MAXIMUM}");
+            HiLoGame.Hint();
+            while (HiLoGame.GetPot() > 0)  // while we still have money in the pot (Cant play if we have no money)
+            {
+                Console.WriteLine("Press h for higher, l for lower, ? to buy a hint");
+                Console.WriteLine($"Or any other key to quit with {HiLoGame.GetPot()}");
+                // look out for the key being entered
+                char key = Console.ReadKey(true).KeyChar;
+                // decisions for the key that the user entered
+                if (key == 'h') HiLoGame.Guess(true);  // if it is true that the next number is greater than the current number
+                else if (key == 'l') HiLoGame.Guess(false);  // if number is less than current number
+                else if (key == 'q') HiLoGame.Hint();
+                else return;
+            }
+            // if we escape the while loop then it means we ran out of money
+            Console.WriteLine("The pot is empty... Byyeeeee");
+        }
+    }
+}
